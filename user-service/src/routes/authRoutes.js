ab15df93c98392d9364411d605323492690a89b0
Reshan123 <reshangomis@gmail.com>
2026-03-21 22:11:19 +0530

    feat(auth): remove unused authentication middleware import

--- user-service/src/routes/authRoutes.js
+++ user-service/src/routes/authRoutes.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const { authenticate } = require("../middleware/auth");
 
 /**
  * @swagger
